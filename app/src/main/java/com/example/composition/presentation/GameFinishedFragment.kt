a2ecda1000f59a904b258211b52d06a9dbe83b6a
Maxim <jybo@mail.ru>
2024-08-27 09:45:00 +1000

    Step-3 Presentation (добавлены layout-ы для работы)

---
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -0,0 +1,19 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class GameFinishedFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.finished_game_fragment, container, false)
+    }
+}
