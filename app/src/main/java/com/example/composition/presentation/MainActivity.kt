38b1ecbe08a1d53f378b541be0a24acc729c8c12
Maxim <jybo@mail.ru>
2024-09-03 23:33:07 +1000

    Step-3 Presentation (покрашен navigationBar )

--- app/src/main/java/com/example/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.composition.presentation
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +12,7 @@ import com.example.composition.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(navigationBarStyle = SystemBarStyle.light(Color.RED, Color.TRANSPARENT))
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
